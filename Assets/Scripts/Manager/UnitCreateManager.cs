509d67c09e9db814d36424ccc55a60f39364a067
냠냠 <lanilani7661@gmail.com>
2022-03-31 01:02:09 +0900

    UnitSpawn
    UnitSpawn

---
+++ Assets/Scripts/Manager/UnitCreateManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitCreateManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject unitObj;
+
+    InputManager inputManager;
+    
+
+    public void UnitCrate()
+    {
+        // 유닛 생산에 필요한것
+
+        // 유닛 오브젝트 가져오기?
+
+        // 유닛 오브젝트를 소환하였을때 소환될 위치 가져오기?
+
+        Instantiate(unitObj, transform.position, Quaternion.identity);
+        
+    }
+
+}
